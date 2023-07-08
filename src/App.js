c4d923d7150fd525953d91e9c013fdc7462c3b39
John Kenneth Hugo <john.hugo.ece.ect@gmail.com>
2023-07-09 01:03:40 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className = "timer-container">
+        <span>00</span>
+        <span>:</span>
+        <span>00</span>
+        <span>:</span>
+        <span>00</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
